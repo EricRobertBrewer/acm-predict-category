2ec5e460d536b9de38ab178b58f23e25d4487de2
Eric Brewer <eric.r.brewer@gmail.com>
2018-05-30 10:33:50 -0600

    Add GRU.

--- acm1.py
+++ acm1.py
@@ -2,7 +2,7 @@ import os
 from gensim.models.doc2vec import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
+from keras.layers import Dense, Dropout, Activation, LSTM
 from keras.preprocessing.text import Tokenizer
 from sklearn.preprocessing import LabelBinarizer
 import numpy as np
@@ -39,6 +39,8 @@ def keras_text_ori(lines):
     docs = list()
     for line in lines:
         attributes = line.split('|')
+        if len(attributes) < 8:
+            continue
         paragraph = attributes[7] + " " + attributes[1]
         docs.append(paragraph)
     return docs
@@ -55,10 +57,11 @@ def doc2vec_process(lines):
             continue
         words = "".join([c for c in attributes[7] if c in string.ascii_letters or c in string.whitespace])
         words = words.split(' ')
-        while "" in words: words.remove("")
+        while "" in words:
+            words.remove("")
         tags = list()
         tags.append(i)
-        tagged_list.append(TaggedDocument(words,tags))
+        tagged_list.append(TaggedDocument(words, tags))
         i += 1
     vector_size = 15
     model = Doc2Vec(tagged_list, vector_size=vector_size, alpha=0.025, min_alpha=0.025)
@@ -83,27 +86,27 @@ def doc2vec_process(lines):
 def main():
     with open('acm_result1-6.txt', encoding="utf8") as f:  # Change the input file here
         lines = f.readlines()
-    with open('test.txt') as f:  # Change the text file here
-        tests = f.readlines()
-    docs = keras_text(lines)
-    tests1 = keras_text_ori(tests)
+        docs = keras_text(lines)
+        del lines
+    with open('test.txt') as f:  # Change the test file here
+        lines = f.readlines()
+        tests = keras_text_ori(lines)
+        del lines
 
     t = Tokenizer()
     t.fit_on_texts(docs)
-    t.fit_on_texts(tests1)
+    t.fit_on_texts(tests)
     print(t.word_counts)
 
     encoded_docs = t.texts_to_matrix(docs, mode='count')
-    encoded_test = t.texts_to_matrix(tests1, mode='count')
+    encoded_test = t.texts_to_matrix(tests, mode='count')
 
     vector_list = []
     test_vector_list = []
     for doc in encoded_docs:
         vector_list.append(doc)
-
     for test in encoded_test:
         test_vector_list.append(test)
-
     label_list = []
     for line in lines:
         attributes = line.split('|')
@@ -137,9 +140,12 @@ def main():
     # x_test = tokenizer.sequences_to_matrix(x_test, mode='binary')
     # print('x_train shape:', x_train.shape)
     # print('x_test shape:', x_test.shape)
-    # ["socialandprofessionaltopics", "appliedcomputing", "computingmethodologies", "human-centeredcomputing", "securityandprivacy"
-    #                                 , "informationsystems", "mathematicsofcomputing", "theoryofcomputation", "softwareanditsengineering", "hardware"
-    #                                             , "generalandreference"]
+    # ["socialandprofessionaltopics", "appliedcomputing",
+    # "computingmethodologies", "human-centeredcomputing"
+    # "securityandprivacy", "informationsystems",
+    # "mathematicsofcomputing", "theoryofcomputation",
+    # "softwareanditsengineering", "hardware",
+    # "generalandreference"]
     print('Convert class vector to binary class matrix '
           '(for use with categorical_cross entropy)')
     encoder = LabelBinarizer()
@@ -150,25 +156,29 @@ def main():
     print('Building model...')
     # This Section is the neural network
     model = Sequential()
-    model.add(Dense(512, activation='relu', input_shape=(len(vector_list[0]),)))
+    model.add(LSTM(20,
+                   activation='relu',
+                   recurrent_activation='hard_sigmoid',
+                   input_shape=(len(vector_list[0]),)))
+    # model.add(Dense(512, activation='relu', input_shape=(len(vector_list[0]),)))
     model.add(Dropout(0.5))
-    model.add(Dense(512))
+    model.add(Dense(num_classes))
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy',
                   optimizer='sgd',
                   metrics=['categorical_accuracy'])
 
-    history = model.fit(np.array(x_train), np.array(y_train),
-                        batch_size=batch_size,
-                        epochs=epochs,
-                        verbose=1,
-                        validation_split=0.1)
+    _ = model.fit(np.array(x_train), np.array(y_train),
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  verbose=1,
+                  validation_split=0.1)
 
     result = model.predict_classes(np.array(x_test))
     print(result)
     result = model.predict_classes(np.array(test_vector_list))
-    fd = open('result2.txt', "w+")  # Output file of the test result
+    fd = open('result3.txt', "w+")  # Output file of the test result
     result1 = np.nditer(result)
     num = 0
     for line in result1:
@@ -215,7 +225,7 @@ def main():
     fd.close()
 
     # serialize model to JSON
-    model.save('my_model.h5')
+    model.save('my_model3.h5')
     # serialize weights to HDF5
     print("Saved model to disk")
     score = model.evaluate(np.array(x_test), np.array(y_test),
--- acm_lstm.py
+++ acm_lstm.py
@@ -1,43 +1,52 @@
 import os
 import io
 import sys
+import re
 import pandas as pd
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Activation, Dense, Input, LSTM, Embedding, Dropout, GlobalMaxPool1D, Bidirectional
+from keras.layers import Activation, Dense, Input, GRU, Embedding, Dropout, GlobalMaxPool1D, Bidirectional
 from keras.models import Model
 from sklearn.preprocessing import LabelBinarizer
 import stop_words
-from stemming.porter2 import stem
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+LOGS_FOLDER = 'logs'
+MODELS_FOLDER = 'models'
 TRAIN_FILE = 'train.csv'
 # This can depend on if the embedding contains cased words or not.
 DO_LOWER_CASE = True
-# When True, perform stop word removal and stemming.
-DO_PREPROCESS_TEXT = False
+# When True, remove punctuation.
+DO_REMOVE_PUNCTUATION = True
+# When True, perform stop word removal.
+DO_REMOVE_STOP_WORDS = True
+# When True, perform stemming.
+DO_STEMMING = False
 # GloVe: https://nlp.stanford.edu/projects/glove/
-EMBEDDING_FILE = os.path.join('..', 'embeddings', 'glove.6B.50d.txt')
+EMBEDDING_GLOVE_50 = 'glove.6B.50d.txt'
+EMBEDDING_GLOVE_300 = 'glove.6B.300d.txt'
 # fastText: https://fasttext.cc/docs/en/english-vectors.html
-# EMBEDDING_FILE = os.path.join('..', 'embeddings', 'wiki-news-300d-1M.vec')
+EMBEDDING_FASTTEXT_300 = 'wiki-news-300d-1M.vec'
+EMBEDDING_FILES = [os.path.join('..', 'embeddings', e) for e in [EMBEDDING_GLOVE_300]]
+DO_TRAIN_EMBEDDING = False
 
 
-def preprocess_text(sentences):
+def remove_stop_words(sentences):
     """
     Perform stop-word removal and stemming.
     """
-    lines = list()
+    _sentences = list()
     english_stop_words = stop_words.get_stop_words('en')
     for sentence in sentences:
-        sentence_words = [stem(word) for word in sentence.split() if word not in english_stop_words]
-        lines.append(' '.join(sentence_words))
-    return lines
+        words = [word for word in sentence.split() if word not in english_stop_words]
+        _sentences.append(' '.join(words))
+    return _sentences
 
 
 def get_embedding(tokenizer, file_name=None, max_features=20000):
     """
-    Get the embedding matrix
+    Get the embedding matrix.
     :param tokenizer: The Tokenizer which tokenized our text.
     :param file_name: The path to the embedding file.
     :param max_features: The upper limit on the number of words (vectors) to include in the matrix.
@@ -46,7 +55,7 @@ def get_embedding(tokenizer, file_name=None, max_features=20000):
     word_index = tokenizer.word_index
     word_count = min(max_features, len(word_index))
     if file_name is None:
-        embed_size = 128
+        embed_size = 64
         return embed_size, np.random.uniform(-1, 1, (word_count, embed_size))
     if 'glove' in file_name:
         with open(file_name, 'r', encoding='utf8') as fd:
@@ -82,7 +91,7 @@ def load_vectors(file_name):
     fin = io.open(file_name, 'r', encoding='utf-8', newline='\n', errors='ignore')
     # The first line contains the number of rows (n) and the dimensionality (d)
     n, d = map(int, fin.readline().split())
-    data = {}
+    data = dict()
     for line in fin:
         tokens = line.rstrip().split(' ')
         data[tokens[0]] = map(float, tokens[1:])
@@ -90,16 +99,31 @@ def load_vectors(file_name):
     return data
 
 
-def get_model(maxlen, embedding_matrix, label_count):
+def get_spread(labels):
+    spread = dict()
+    for label in labels:
+        try:
+            spread[label] += 1
+        except KeyError:
+            spread[label] = 1
+    return spread
+
+
+def get_model(maxlen, embedding_matrix, time_steps, label_count):
     inp = Input(shape=(maxlen,))
-    input_dim = embedding_matrix.shape[0]
-    output_dim = embedding_matrix.shape[1]
-    x = Embedding(input_dim, output_dim, weights=[embedding_matrix])(inp)
-    x = Bidirectional(LSTM(50, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(x)
+    if embedding_matrix is not None:
+        input_dim = embedding_matrix.shape[0]
+        output_dim = embedding_matrix.shape[1]
+        x = Embedding(input_dim, output_dim, weights=[embedding_matrix], trainable=DO_TRAIN_EMBEDDING)(inp)
+    else:
+        x = inp
+    x = Bidirectional(GRU(time_steps, return_sequences=True, dropout=0.2, recurrent_dropout=0.1))(x)
+    # x = Bidirectional(GRU(64, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(x)
     x = GlobalMaxPool1D()(x)
-    x = Dense(50, activation='relu')(x)
-    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.2)(x)
     x = Dense(label_count)(x)
+    x = Dropout(0.2)(x)
     x = Activation('softmax')(x)
     return Model(inputs=inp, outputs=x)
 
@@ -108,9 +132,9 @@ def main():
     # columns = ['class', 'title', 'u1', 'authors', 'source', 'publisher', 'citations', 'abstract', 'keywords']
     data = pd.read_csv(TRAIN_FILE)
     data.dropna(subset=['class', 'title', 'abstract'])
-    print('# of rows: {}'.format(len(data)))
+    print('Rows of data: {}'.format(len(data)))
     label_count = data['class'].nunique()
-    print('# of unique labels: {}'.format(label_count))
+    print('Unique labels: {}'.format(label_count))
     labels = np.array(data['class'])
     titles = list(data['title'])
     abstracts = list(data['abstract'])
@@ -118,15 +142,16 @@ def main():
         sentences = [titles[i].lower() + ' ' + abstracts[i].lower() for i in range(len(titles))]
     else:
         sentences = [titles[i] + ' ' + abstracts[i] for i in range(len(titles))]
-    if DO_PREPROCESS_TEXT:
-        lines = preprocess_text(sentences)
-    else:
-        lines = np.array(sentences)
+    if DO_REMOVE_PUNCTUATION:
+        sentences = [re.sub(r'[~`!@#$%^&*()_\-+={}\[\]|\\:;"\'<>,.?/]+', ' ', sentence) for sentence in sentences]
+    if DO_REMOVE_STOP_WORDS:
+        sentences = remove_stop_words(sentences)
+    lines = np.array(sentences)
     # Shuffle the labels and lines.
     permutation = np.random.permutation(labels.shape[0])
     labels = labels[permutation]
     lines = lines[permutation]
-    # Split the data.
+    # Split the data for training and testing.
     train_ratio = 0.8
     val_ratio = 0.25  # Proportion of the TRAINING data, not of the entire data set.
     train_end = int(train_ratio * len(lines))
@@ -134,44 +159,92 @@ def main():
     train_lines = lines[:train_end]
     test_labels = labels[train_end:]
     test_lines = lines[train_end:]
+    # Print class spreads in each data set.
+    val_start = int((1 - val_ratio) * train_end)
+    train_spread = get_spread(train_labels[:val_start])
+    test_spread = get_spread(test_labels)
+    val_spread = get_spread(train_labels[val_start:])
+    longest_label = max(len(label) for label in train_spread.keys())
+    for label in train_spread.keys():
+        print('{}{}: TR:{} VAL:{} TS:{}'.format(label,
+                                                ''.join(' ' for _ in range(longest_label - len(label))),
+                                                train_spread[label] / val_start,
+                                                val_spread[label] / (train_end - val_start),
+                                                test_spread[label] / len(test_labels)))
     # Pre-process; tokenize the text and transform it into [padded] sequences for the RNN.
     # See: https://www.kaggle.com/sbongo/for-beginners-tackling-toxic-using-keras
     # See: https://www.kaggle.com/jhoward/improved-lstm-baseline-glove-dropout
-    max_features = 20000  # The maximum number of total unique words to use.
-    maxlen = 100  # The maximum number of words to pass through the network at a time.
+    max_features = 10000  # The maximum number of total unique words to use.
+    maxlen = 50  # The maximum number of words to pass through the network at a time.
     tokenizer = Tokenizer(num_words=max_features)
     tokenizer.fit_on_texts(train_lines)
     train_sequences = tokenizer.texts_to_sequences(train_lines)
     test_sequences = tokenizer.texts_to_sequences(test_lines)
     x_train = pad_sequences(train_sequences, maxlen=maxlen)
     x_test = pad_sequences(test_sequences, maxlen=maxlen)
-    # Prepare the embedding.
-    embed_size, embedding_matrix = get_embedding(tokenizer, EMBEDDING_FILE, max_features)
+    # Prepare the embedding(s).
+    embed_sizes, embedding_matrices = list(), list()
+    for embedding_file in EMBEDDING_FILES:
+        embed_size, embedding_matrix = get_embedding(tokenizer, embedding_file, max_features)
+        embed_sizes.append(embed_size)
+        embedding_matrices.append(embedding_matrix)
+    embed_size, embedding_matrix = None, None
+    if len(embed_sizes) > 0 and len(embedding_matrices) > 0:
+        embed_size, embedding_matrix = embed_sizes[0], embedding_matrices[0]
+        for i in range(1, len(embed_sizes)):
+            if embed_sizes[i] != embed_size:
+                raise Exception('Embeddings sizes are different: {} and {}.'
+                                .format(embed_size, embed_sizes[i]))
+            if embedding_matrices[i].shape != embedding_matrix.shape:
+                raise Exception('Embedding matrices have different shapes: {} and {}'
+                                .format(embedding_matrix.shape, embedding_matrices[i].shape))
+            # Concatenate all embeddings matrices.
+            embedding_matrix += embedding_matrices[i]
     # Transform the labels into a one-hot encoding.
     encoder = LabelBinarizer()
     y_train = encoder.fit_transform(train_labels)
     y_test = encoder.fit_transform(test_labels)
     # Build the model.
-    model = get_model(maxlen, embedding_matrix, label_count)
+    time_steps = 32  # Should probably be less than `maxlen`.
+    model = get_model(maxlen, embedding_matrix, time_steps, label_count)
+    print(model.summary())
+    optimizer = 'rmsprop'
     model.compile(loss='categorical_crossentropy',
-                  optimizer='sgd',
+                  optimizer=optimizer,
                   metrics=['categorical_accuracy'])
-    model.fit(x_train,
-              y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              validation_split=val_ratio)
+    history = model.fit(x_train,
+                        y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        validation_split=val_ratio)
+    # Save the model and the history.
+    save_str = 'gru1-{}-g{}-{}{}{}{}{}-{}-{}'.format(
+        maxlen,
+        '-rawembedding' if not DO_TRAIN_EMBEDDING else '',
+        time_steps,
+        '-lower' if DO_LOWER_CASE else '',
+        '-nopunc' if DO_REMOVE_PUNCTUATION else '',
+        '-nostop' if DO_REMOVE_STOP_WORDS else '',
+        '-stem' if DO_STEMMING else '',
+        optimizer,
+        epochs)
+    print('Saving model `{}.h5`...'.format(save_str))
+    model.save(os.path.join(MODELS_FOLDER, '{}.h5'.format(save_str)))
+    print('Saving training history `history-{}.txt`...'.format(save_str))
+    with open(os.path.join(LOGS_FOLDER, 'history-{}.txt'.format(save_str)), 'w') as fd:
+        for key in history.history.keys():
+            values = history.history.get(key)
+            fd.write(key + ' ' + ' '.join(str(value) for value in values) + '\n')
     # Test.
     # predictions = model.predict([test_sequences], batch_size=1024, verbose=1)
-    model.save('lstm3.h5')
-    score = model.evaluate(x_test, y_test, batch_size=1024, verbose=1)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
+    test_loss, test_accuracy = model.evaluate(x_test, y_test, batch_size=1024, verbose=1)
+    print('Test loss:', test_loss)
+    print('Test accuracy:', test_accuracy)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2 or len(sys.argv) > 3:
         raise Exception('Usage: <epochs> [<batch-size>]')
     epochs = int(sys.argv[1])
-    batch_size = sys.argv[2] if len(sys.argv) > 2 else 32
+    batch_size = int(sys.argv[2]) if len(sys.argv) > 2 else 32
     main()
